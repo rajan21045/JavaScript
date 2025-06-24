17a797745cdb708408478dd14acaaf95176e46eb
Rajan Poudel <raajan.works@gmail.com>
2025-06-24 14:28:36 +0545

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,46 @@
+let num1 = 12;
+let num2 = 28;
+let sum = num1 + num2;
+console.log("Sum:", sum);
+
+
+let num3 = 15;
+let num4 = 5;
+let difference = num3 - num4;
+console.log("Difference:", difference);
+
+
+let num5 = 20;
+let num6 = 4;
+let quotient = num5 / num6;
+console.log("Quotient:", quotient);
+
+
+let num7 = 7;
+let num8 = 3;
+let product = num7 * num8;
+console.log("Product:", product);
+
+
+let num9 = 10;
+let num10 = 3;
+let remainder = num9 % num10;
+console.log("Remainder:", remainder);
+
+
+let num11 = 8;
+num11++;
+console.log("Incremented Value:", num11);
+
+
+let num12 = 5;
+num12--;
+console.log("Decremented Value:", num12);
+
+
+let num13 = 10;
+let num14 = Math.pow(num13,2);
+
+
+let x = (100 + 50) * 3;
+console.log("Math Power Of X ", x);
