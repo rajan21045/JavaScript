af6277fed824d527dde0bd15e2ee5038d7ec9b1e
Rajan Poudel <raajan.works@gmail.com>
2026-04-28 22:04:20 +0545

    Update variable.js

--- variable.js
+++ variable.js
@@ -3,12 +3,11 @@
 /*
 
 Variables are Containers for Storing Data
-    JavaScript Variables can be declared in 4 ways:
-
-1. Automatically
-2. Using var
-3. Using let
-4. Using const
+JavaScript Variables can be declared in 4 ways:
+    1. Automatically
+    2. Using var
+    3. Using let
+    4. Using const
  
 */
 
